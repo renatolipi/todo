7685fcf5690a5e7dde08497ebff60ac8c154b303
Renato Lipi <renatolipi@gmail.com>
2020-04-15 17:40:22 -0300

    Create lightsail-compose-2.sh

---
+++ lightsail-compose-2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# make it so you don't need to sudo to run docker commands
+usermod -aG docker ubuntu
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# copy the dockerfile into /srv/docker 
+# if you change this, change the systemd service file to match
+# WorkingDirectory=[whatever you have below]
+mkdir /srv/docker
+curl -o /srv/docker/docker-compose-2.yml https://raw.githubusercontent.com/renatolipi/todo/develop/docker-compose-2.yml
+
+# copy in systemd unit file and register it so our compose file runs 
+# on system restart
+curl -o /etc/systemd/system/docker-compose-app-2.service https://raw.githubusercontent.com/renatolipi/todo/develop/docker-compose-app-2.service
+systemctl enable docker-compose-app-2
+
+# start up the application via docker-compose
+docker-compose -f /srv/docker/docker-compose-2.yml up -d
